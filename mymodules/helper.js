7b2a3f96a9fd510f5bfc75dcabc4c337afd242b8
Georgegoodey <george@goodey.co.uk>
2025-04-05 18:54:13 +0100

    feat: softmax shuffle and stub search changes

--- mymodules/helper.js
+++ mymodules/helper.js
@@ -11,39 +11,44 @@ function makeid(length){
 }
 
 function shuffle(array,players,maxSongs){
-    let currentIndex = array.length;
+    // Fill array with maxSongs e.g. [3,3,3]
+    // Counts songs left to be added for each player
+    let p_counts = Array(players.length).fill(maxSongs)
+
+    let shuffled_songs = []
+
+    // While shuffled_songs is incomplete
+    while (shuffled_songs.length < maxSongs*players.length) {
+
+        // Calculate softmax of counts - Leaving 0 as 0
+        let p_exp = p_counts.map(p => (p === 0 ? 0 : Math.exp(p)))
+        let p_sum = p_exp.reduce((p_1,p_2) => p_1 + p_2, 0)
+        let p_softmax = p_exp.map(p => (p === 0 ? 0 : p / p_sum))
+
+        // Get index using weighted selection
+        let p_index = weightedRandom(p_softmax);
+        let player = players[p_index]
+        p_counts[p_index] -= 1
+        
+        let songs = array.filter((song) => song.playerID == player)
+        let song = songs[Math.floor((songs.length) * Math.random())]
+
+        // Remove chosen song from array and add to output
+        array = array.filter(s => s !== song)
+        shuffled_songs.push(song)
+    }
     
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+    return shuffled_songs
+}
+
+function weightedRandom(probabilities) {
+    const rand = Math.random();
+    let sum = 0;
+    for (let i = 0; i < probabilities.length; i++) {
+        sum += probabilities[i];
+        if (rand < sum) return i;
     }
-    let rounds = [];
-    for (let i = 0; i < maxSongs; i++) {
-        rounds.push([]);
-    }
-    for (let i = 0; i < players.length; i++) {
-        let songs = array.filter((song) => song.playerID == players[i])
-        for (let j = 0; j < songs.length; j++) {
-            let select = Math.random()
-            if (j > 0 && select < 0.2) {
-                rounds[j-1].push(songs[j])
-                rounds[j].splice(rounds[j].indexOf([]),1)
-            } else if (j < songs.length-1 && select > 0.8) {
-                rounds[j+1].push(songs[j])
-                rounds[j].splice(rounds[j].indexOf([]),1)
-            } else {
-                rounds[j].splice((rounds[j].length+1) * Math.random() | 0, 0, songs[j])
-            }
-        }
-    }
-    return rounds.flat();
+    return probabilities.length - 1;
 }
 
 module.exports = {makeid, shuffle}
